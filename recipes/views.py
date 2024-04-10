78ff9b59892014b91e1337295fa18ef1d7f5b48c
vitor santos <vitorsantoos96@gmail.com>
2024-04-10 15:49:03 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -5,10 +5,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Vitor santoos',
     })
-
-def contato(request):
-    return render(request, 'me-apague/temp.html')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
