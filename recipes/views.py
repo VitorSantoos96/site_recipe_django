69bb63c34aea2986d2e17a107692032dae1dc5c1
vitor santos <vitorsantoos96@gmail.com>
2024-04-20 16:27:27 -0300

    Atualiza;'ao aula 04/20/2024

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from .models import Recipe
+
 
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
     })
 
 
-def recipe(request,id):
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id,).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+    })
+
+
+def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': Recipe,
         'is_detail_page': True,
     })
