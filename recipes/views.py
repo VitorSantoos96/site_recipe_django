3b083dcc01ce51e7a654e2b448a06ef25b920246
vitor santos <vitorsantoos96@gmail.com>
2024-04-23 15:58:33 -0300

    Atualização aula 23/04/24 OBS: tem que resolver a URL recipe

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from utils.recipes.factory import make_recipe
-from .models import Recipe
+
+
+from recipes.models import Recipe
 
 
 def home(request):
@@ -11,14 +13,18 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id,).order_by('-id')
+    recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id,).order_by('-id'),)
+        
+    
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes[0].category.name}  - Category | ',
     })
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(pk=id,).order_by('-id').first,
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': Recipe,
+        'recipe': recipe,
         'is_detail_page': True,
     })
