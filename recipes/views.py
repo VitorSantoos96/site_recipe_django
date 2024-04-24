93b5bb660e0aa8f40e6c474b3c3b3b9d6e94adeb
vitor santos <vitorsantoos96@gmail.com>
2024-04-24 14:38:54 -0300

    autliazação aula 24/04/2024

--- recipes/views.py
+++ recipes/views.py
@@ -22,9 +22,9 @@ def category(request, category_id):
     })
 
 
-def recipe(request, id):
+def recipe(request, id,):
     recipe = Recipe.objects.filter(pk=id,).order_by('-id').first,
-    return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': recipe,
+    return render(request,'recipes/pages/recipe-view.html', context={
+        'recipes': recipe,
         'is_detail_page': True,
     })
