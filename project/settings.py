4068dff5cabba56831a166bfb69cbd52fb30e850
vitor santos <vitorsantoos96@gmail.com>
2024-05-14 16:37:43 -0300

    atualização aula 14/05/24

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gel$&k=7i^oaw4u@rjxko3fpok@!z0)3&9@s&j2#w-=nscrjy#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
